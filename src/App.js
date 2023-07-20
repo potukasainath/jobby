1b5e573ae0390296f5c4d5753011000f0ac11602
sainath <spreddy846@gmail.com>
2023-07-20 16:18:46 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+import AboutJob from './components/AboutJob'
+import AllJobs from './components/AllJobs'
+import NotFound from './components/NotFound'
+import LoginFrom from './components/LoginForm'
+import './App.css'
+
+// Replace your code here
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginFrom} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/jobs" component={AllJobs} />
+    <ProtectedRoute exact path="/jobs':id" component={AboutJob} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,52 @@
+import {withRouter, Link} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {BsBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
+import Cookies from 'js-cookie'
+import './index.css'
+
+const Header = props => {
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="header-container">
+      <u1 className="list-container">
+        <li className="logo-container">
+          <Link to="/" className="nav-link">
+            <img
+              className="logo"
+              alt="website logo"
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            />
+          </Link>
+        </li>
+        <li className="nav-links-container">
+          <Link to="/" className="nav-link">
+            <h1 className="nav-to-name">Home</h1>
+            <AiFillHome className="nav-icon" />
+          </Link>
+          <Link to="/jobs" className="nav-link">
+            <h1 className="nav-to-name">Jobs</h1>
+            <BsBriefcaseFill className="nav-icon" />
+          </Link>
+        </li>
+        <li className="nav-button-container">
+          <FiLogOut className="nav-icon" onClick={onClickLogout} />
+          <button
+            className="logout-button"
+            type="button"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </li>
+      </u1>
+    </nav>
+  )
+}
+
+export default withRouter(Header)
